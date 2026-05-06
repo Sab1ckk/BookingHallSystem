334915fa26e184109e8dcd8a4d2c1a8522ffb862
Mazzzuta <maksimberilko@gmail.com>
2026-05-06 09:59:22 +0900

    Added CRUD

--- BookingSystem/BookingSystem/ViewModel/PositionViewModel.cs
+++ BookingSystem/BookingSystem/ViewModel/PositionViewModel.cs
@@ -1,12 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BookingSystem.Help;
 
 namespace BookingSystem.ViewModel
 {
-    internal class PositionViewModel
+    public class PositionViewModel : INotifyPropertyChanged
     {
+        private readonly SystemModel _context;
+        private string _name;
+        private string _description;
+        private Positions _selectedPosition;
+
+        private ObservableCollection<Positions> _positions;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+        public Positions SelectedPosition
+        {
+            get => _selectedPosition;
+            set
+            {
+                _selectedPosition = value;
+                if (value != null)
+                {
+                    Name = value.Name;
+                    Description = value.Description;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Positions> Positions
+        {
+            get => _positions;
+            set { _positions = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand UpdateCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public PositionViewModel()
+        {
+            _context = new SystemModel();
+            LoadData();
+
+            AddCommand = new RelayCommand(_ => AddPosition());
+            UpdateCommand = new RelayCommand(_ => UpdatePosition(), _ => SelectedPosition != null);
+            DeleteCommand = new RelayCommand(_ => DeletePosition(), _ => SelectedPosition != null);
+            ClearCommand = new RelayCommand(_ => ClearForm());
+        }
+
+        private void LoadData()
+        {
+            Positions = new ObservableCollection<Positions>(_context.Positions.ToList());
+        }
+
+        private void AddPosition()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                return;
+
+            var position = new Positions
+            {
+                Name = Name,
+                Description = Description
+            };
+
+            _context.Positions.Add(position);
+            _context.SaveChanges();
+            LoadData();
+            ClearForm();
+        }
+
+        private void UpdatePosition()
+        {
+            if (SelectedPosition == null) return;
+
+            SelectedPosition.Name = Name;
+            SelectedPosition.Description = Description;
+
+            _context.SaveChanges();
+            LoadData();
+            ClearForm();
+        }
+
+        private void DeletePosition()
+        {
+            if (SelectedPosition == null) return;
+
+            if (SelectedPosition.Employees.Any())
+            {
+                System.Windows.MessageBox.Show("Нельзя удалить должность, к которой привязаны сотрудники!",
+                    "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                return;
+            }
+
+            var result = System.Windows.MessageBox.Show("Удалить выбранную должность?", "Подтверждение",
+                System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Question);
+
+            if (result == System.Windows.MessageBoxResult.Yes)
+            {
+                _context.Positions.Remove(SelectedPosition);
+                _context.SaveChanges();
+                LoadData();
+                ClearForm();
+            }
+        }
+
+        private void ClearForm()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            SelectedPosition = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
--- BookingSystem/BookingSystem/ViewModel/RoomViewModel.cs
+++ BookingSystem/BookingSystem/ViewModel/RoomViewModel.cs
@@ -1,12 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BookingSystem.Help;
 
-namespace BookingSystem.View
+namespace BookingSystem.ViewModel
 {
-    internal class RoomViewModel
+    public class RoomViewModel : INotifyPropertyChanged
     {
+        private readonly SystemModel _context;
+        private string _name;
+        private int _capacity;
+        private Rooms _selectedRoom;
+        private ObservableCollection<Rooms> _rooms;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public int Capacity
+        {
+            get => _capacity;
+            set { _capacity = value; OnPropertyChanged(); }
+        }
+
+        public Rooms SelectedRoom
+        {
+            get => _selectedRoom;
+            set
+            {
+                _selectedRoom = value;
+                if (value != null)
+                {
+                    Name = value.Name;
+                    Capacity = value.Capacity;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Rooms> Rooms
+        {
+            get => _rooms;
+            set { _rooms = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand UpdateCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public RoomViewModel()
+        {
+            _context = new SystemModel();
+            LoadRooms();
+            AddCommand = new RelayCommand(_ => AddRoom());
+            UpdateCommand = new RelayCommand(_ => UpdateRoom(), _ => SelectedRoom != null);
+            DeleteCommand = new RelayCommand(_ => DeleteRoom(), _ => SelectedRoom != null);
+            ClearCommand = new RelayCommand(_ => ClearForm());
+        }
+
+        private void LoadRooms()
+        {
+            Rooms = new ObservableCollection<Rooms>(_context.Rooms.ToList());
+        }
+
+        private void AddRoom()
+        {
+            if (string.IsNullOrWhiteSpace(Name) || Capacity <= 0)
+                return;
+
+            var room = new Rooms
+            {
+                Name = Name,
+                Capacity = Capacity
+            };
+
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+            LoadRooms();
+            ClearForm();
+        }
+
+        private void UpdateRoom()
+        {
+            if (SelectedRoom == null) return;
+
+            SelectedRoom.Name = Name;
+            SelectedRoom.Capacity = Capacity;
+            _context.SaveChanges();
+            LoadRooms();
+            ClearForm();
+        }
+
+        private void DeleteRoom()
+        {
+            if (SelectedRoom == null) return;
+
+            _context.Rooms.Remove(SelectedRoom);
+            _context.SaveChanges();
+            LoadRooms();
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            Name = string.Empty;
+            Capacity = 0;
+            SelectedRoom = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
